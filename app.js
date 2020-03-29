eaa49f2465760b865f26782d37f302bc66bdf765
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-29 01:24:02 -0400

    + Aplicando la configuracion del servidor, body-parser

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
+// Configuration
+var serverConfig = require("./config/serverConfig");
+const host = serverConfig.HOST;
+const port = serverConfig.PORT;
+
 var express = require('express');
+var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Listen
+app.listen(port, host, function () {
+    console.log(`Server is running on http://${host}:${port}`);
+});
 
 module.exports = app;
