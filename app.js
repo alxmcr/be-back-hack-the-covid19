ed6141e407eb15c51affd732ecc42bebd6b2ba24
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-29 19:21:01 -0400

    ! Trabajando con la creacion de los accesos a las rutas

--- app.js
+++ app.js
@@ -2,16 +2,13 @@
 const serverConfig = require(__dirname + '/config/server-config.json');
 const host = serverConfig.HOST;
 const port = serverConfig.PORT;
+const versionAPI = serverConfig.API_VERSION;
 
 var express = require('express');
 var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-
-// Definicion de las rutas
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/v1/users');
 
 var app = express();
 
@@ -22,12 +19,38 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Definicion de las rutas
+loadRoutes(app, versionAPI);
 
 // Listen
 app.listen(port, host, function () {
     console.log(`Server is running on http://${host}:${port}`);
 });
 
+function loadRoutes(app, versionAPI) {
+    var indexRouter = require("./routes/index");
+    // API: Entities
+    var rutasRouter = require(`./routes/api/${versionAPI}/rutas`);
+    var busesRouter = require(`./routes/api/${versionAPI}/buses`);
+    var viajesRouter = require(`./routes/api/${versionAPI}/viajes`);
+    var usuariosRouter = require(`./routes/api/${versionAPI}/usuarios`);
+    var usuariosEnfermosRouter = require(`./routes/api/${versionAPI}/usuariosEnfermos`);
+
+    // API: Relationships
+    var tienesRouter = require(`./routes/api/${versionAPI}/relationships/tienes`);
+
+    app.use("/", indexRouter);
+
+    // API: Entities
+    app.use(`/api/${versionAPI}/rutas`, rutasRouter);
+    app.use(`/api/${versionAPI}/buses`, busesRouter);
+    app.use(`/api/${versionAPI}/viajes`, viajesRouter);
+    app.use(`/api/${versionAPI}/usuarios`, usuariosRouter);
+    app.use(`/api/${versionAPI}/usuarios-enfermos`, usuariosEnfermosRouter);
+    // API: Relationships
+    app.use(`/api/${versionAPI}/tienes`, tienesRouter);
+}
+
 module.exports = app;
+
