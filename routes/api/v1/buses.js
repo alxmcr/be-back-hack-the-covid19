89d35d63bcb867381d17a0c63032cd85d9d3b3a7
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-29 19:59:35 -0400

    ! Trabajando con la creacion de buses y rutas

--- routes/api/v1/buses.js
+++ routes/api/v1/buses.js
@@ -5,9 +5,101 @@ const TYPE_MODEL = "Tabla";
 const NAME_MODEL = "Bus";
 // Sequelize: MODELS
 var models = require("../../../models");
-// GET: Listado de Buses
+// MODEL
+let modelBus = models.Bus;
+// GET: Listado de Registros de tipo Bus
 router.get('/', function (req, res, next) {
-  res.send('Listado de Buses');
+  // OPERATION
+  let operation = "Listar los registros"
+
+  modelBus
+    .findAll()
+    .then(function (listaBus) {
+      const resultOK = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: listaBus
+      }
+      res.status(200).json(resultOK);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
 });
+
+/* GET Buscar un bus por su id */
+router.get("/:bu_bus", function (req, res, next) {
+  // OPERATION
+  let operation = "Buscar un bus por su ID"
+  // ID
+  var id = req.params.bu_bus;
+
+  modelBus
+    .findByPk(id)
+    .then(function (bus) {
+      // Respuesta
+      let respuesta = {};
+
+      if (bus !== undefined && bus !== null) {
+        respuesta = {
+          estado: 200,
+          mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+          data: bus
+        }
+      } else {
+        respuesta = {
+          estado: 404,
+          mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}, no se pudo encontrar el registro con el id ${id}`,
+          data: {}
+        }
+      }
+
+      res.status(respuesta.statusCode).json(respuesta);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* POST: Creacion de un nuevo bus  */
+router.post("/", function (req, res, next) {
+  // OPERATION
+  let operation = "Crear un(a) nuevo bus"
+  // Request Data
+  let dataBusToCreate = req.body;
+
+  modelBus
+    .create(dataBusToCreate)
+    .then(busCreated => {
+      let resCreated = {
+        estado: 201,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: busCreated
+      };
+      res.status(resCreated.statusCode).json(resCreated);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
 
 module.exports = router;
--- routes/api/v1/rutas.js
+++ routes/api/v1/rutas.js
@@ -5,9 +5,101 @@ const TYPE_MODEL = "Tabla";
 const NAME_MODEL = "Ruta";
 // Sequelize: MODELS
 var models = require("../../../models");
-// GET: Listado de Rutas
-router.get('/', function(req, res, next) {
-  res.send('Listado de Rutas');
+// MODEL
+let modelRutas = models.Ruta;
+// GET: Listado de Registros de tipo Rutas
+router.get('/', function (req, res, next) {
+  // OPERATION
+  let operation = "Listar los registros"
+
+  modelRutas
+    .findAll()
+    .then(function (listaRutas) {
+      const resultOK = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: listaRutas
+      }
+      res.status(200).json(resultOK);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* GET Rutascar un Rutas por su id */
+router.get("/:bu_Rutas", function (req, res, next) {
+  // OPERATION
+  let operation = "Buscar un Ruta por su ID"
+  // ID
+  var id = req.params.bu_Rutas;
+
+  modelRutas
+    .findByPk(id)
+    .then(function (Rutas) {
+      // Respuesta
+      let respuesta = {};
+
+      if (Rutas !== undefined && Rutas !== null) {
+        respuesta = {
+          estado: 200,
+          mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+          data: Rutas
+        }
+      } else {
+        respuesta = {
+          estado: 404,
+          mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}, no se pudo encontrar el registro con el id ${id}`,
+          data: {}
+        }
+      }
+
+      res.status(respuesta.statusCode).json(respuesta);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* POST: Creacion de un nuevo Rutas  */
+router.post("/", function (req, res, next) {
+  // OPERATION
+  let operation = "Crear un(a) nuevo Ruta"
+  // Request Data
+  let dataRutaToCreate = req.body;
+  console.log
+
+  modelRutas
+    .create(dataRutaToCreate)
+    .then(rutaCreated => {
+      let resCreated = {
+        estado: 201,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: rutaCreated
+      };
+      res.status(201).json(resCreated);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
 });
 
 module.exports = router;
