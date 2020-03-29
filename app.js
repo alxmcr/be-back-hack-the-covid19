e74e35ad09464ab3b89e4ca1f85921cf6eaf334c
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-29 01:05:11 -0400

    - Quitar lineas que no usaremos

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -12,7 +11,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
