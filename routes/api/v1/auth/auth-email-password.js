113b92c9f824eb43090c69c706172a399afbe91a
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-29 23:23:36 -0400

    ! Trabajando con la autenticacion del usuario por email

--- routes/api/v1/auth/auth-email-password.js
+++ routes/api/v1/auth/auth-email-password.js
@@ -2,26 +2,31 @@ var express = require('express');
 var router = express.Router();
 // Nombre del modelo
 const TYPE_MODEL = "Tabla";
-const NAME_MODEL = "Ruta";
+const NAME_MODEL = "Usuario";
 // Sequelize: MODELS
 var models = require("../../../../models");
 // MODEL
-let modelRutas = models.Ruta;
+let modelUsuario = models.Usuario;
 
 // POST: Iniciar sesion mediante el email y password
 router.post('/', function (req, res, next) {
   // OPERATION
   let operation = "Iniciar sesion mediante el email y password"
 
-  modelRutas
+  // BODY
+  const { us_email, us_password } = req.body;
+
+  modelUsuario
     .findOne({
-      where: { us_username: us_username }
+      where: { us_email, us_password }
     })
-    .then(function (usuario) {
+    .then(function (usuarioFull) {
+      const { us_usuario, us_nombre, us_apellido_paterno, us_apellido_materno } = usuarioFull;
+
       const resultOK = {
         estado: 200,
         mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
-        data: usuario
+        data: { us_usuario, us_nombre, us_apellido_paterno, us_apellido_materno }
       }
       res.status(200).json(resultOK);
     })
