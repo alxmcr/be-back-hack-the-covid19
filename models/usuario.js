d4965764329b1bb5bf69335a11320a233890747b
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 14:13:58 -0400

    * Mejoras en el modelo

--- models/usuario.js
+++ models/usuario.js
@@ -1,90 +1,99 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Usuario = sequelize.define('Usuario', {
-    us_usuario: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
+  const Usuario = sequelize.define(
+    "Usuario",
+    {
+      us_usuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      us_tipo_usuario: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_codigo: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_apellido_paterno: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_apellido_materno: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_carnet_identidad: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+      },
+      us_carnet_lugar_exp: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_direccion_domicilio: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_telefono_fijo: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+      },
+      us_telefono_movil: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+      },
+      us_telefono_emergency: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+      },
+      us_licencia_conducir: {
+        type: DataTypes.STRING
+      },
+      us_categoria_licencia: {
+        type: DataTypes.STRING
+      },
+      us_avatar: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://raw.githubusercontent.com/alxmcr/assets-online-store/master/avatar/profile.png"
+      },
+      us_email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      us_status_covid: {
+        type: DataTypes.BOOLEAN
+      },
+      us_estado: {
+        type: DataTypes.BOOLEAN
+      }
     },
-    us_tipo_usuario: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_codigo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_apellido_paterno: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_apellido_materno: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_carnet_identidad: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
-    },
-    us_carnet_lugar_exp: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_direccion_domicilio: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_telefono_fijo: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
-    },
-    us_telefono_movil: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
-    },
-    us_telefono_emergency: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
-    },
-    us_licencia_conducir: {
-      type: DataTypes.STRING,
-    },
-    us_categoria_licencia: {
-      type: DataTypes.STRING,
-    },
-    us_avatar: {
-      type: DataTypes.STRING,
-      defaultValue:
-        "https://raw.githubusercontent.com/alxmcr/assets-online-store/master/avatar/profile.png"
-    },
-    us_email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    us_status_covid: {
-      type: DataTypes.BOOLEAN,
-    },
-    us_estado: {
-      type: DataTypes.BOOLEAN,
+    {
+      tableName: "Usuario"
     }
-  }, {
-    tableName: "Usuario"
-  });
-  Usuario.associate = function (models) {
-    // associations can be defined here
+  );
+  Usuario.associate = function(models) {
+    // belongsToMany:
+    // adiciona a la tabla RelTiene una foreignKey 'vi_viaje'
+    Usuario.belongsToMany(models.Viaje, {
+      through: models.RelTiene,
+      foreignKey: "us_usuario"
+    });
   };
   return Usuario;
 };
--- models/viaje.js
+++ models/viaje.js
@@ -1,43 +1,54 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Viaje = sequelize.define('Viaje', {
-    vi_viaje: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
+  const Viaje = sequelize.define(
+    "Viaje",
+    {
+      vi_viaje: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      vi_codigo: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      vi_fecha_viaje: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      vi_hora_partida: {
+        type: DataTypes.TIME
+      },
+      vi_hora_llegada: {
+        type: DataTypes.TIME
+      },
+      bu_bus: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      vi_estado: {
+        type: DataTypes.BOOLEAN
+      }
     },
-    vi_codigo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    vi_fecha_viaje: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    vi_hora_partida: {
-      type: DataTypes.TIME,
-    },
-    vi_hora_llegada: {
-      type: DataTypes.TIME,
-    },
-    bu_bus: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    vi_estado: {
-      type: DataTypes.BOOLEAN
+    {
+      tableName: "Viaje"
     }
-  }, {
-    tableName: "Viaje"
-  });
-  Viaje.associate = function (models) {
+  );
+  Viaje.associate = function(models) {
     // associations can be defined here
     Viaje.belongsTo(models.Bus, {
       foreignKey: {
         name: "bu_bus",
         allowNull: false
       }
+    });
+
+    // belongsToMany:
+    // adiciona a la tabla RelTiene una foreignKey 'vi_viaje'
+    Viaje.belongsToMany(models.Usuario, {
+      through: models.RelTiene,
+      foreignKey: "vi_viaje"
     });
   };
   return Viaje;
