c449ba1e20777b10367c943c1ce57170bcb6aa62
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-03-31 13:57:05 -0400

    + Adicionar los servicios REST para la relacion tiene por viaje y Usuario

--- routes/api/v1/relationships/tienes.js
+++ routes/api/v1/relationships/tienes.js
@@ -33,6 +33,70 @@ router.get('/', function (req, res, next) {
     });
 });
 
+/* GET: Buscar todas las relaciones Tiene de un determinado viaje  */
+router.get("/viajes/:vi_viaje", function(req, res, next) {
+  // OPERATION
+  let operation = "Buscar todas las relaciones Tiene de un determinado viaje";
+  // ID
+  let { vi_viaje } = req.params;
+  // Condition
+  let condition = {
+    where: { vi_viaje }
+  };
+
+  modelRelTienes
+    .findAll(condition)
+    .then(function(listaRelTienes) {
+      const resultOK = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: listaRelTienes
+      };
+      res.status(200).json(resultOK);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      };
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* GET: Buscar todas las relaciones Tiene de un determinado Usuario  */
+router.get("/usuarios/:us_usuario", function(req, res, next) {
+  // OPERATION
+  let operation = "Buscar todas las relaciones Tiene de un determinado Usuario";
+  // ID
+  let { us_usuario } = req.params;
+  // Condition
+  let condition = {
+    where: { us_usuario }
+  };
+
+  modelRelTienes
+    .findAll(condition)
+    .then(function(listaRelTienes) {
+      const resultOK = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: listaRelTienes
+      };
+      res.status(200).json(resultOK);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      };
+      res.status(500).send(errorBackend);
+    });
+});
+
 /* POST: Creacion de un nuevo RelTiene  */
 router.post("/", function (req, res, next) {
   // OPERATION
@@ -49,6 +113,70 @@ router.post("/", function (req, res, next) {
         data: RelTieneCreated
       };
       res.status(201).json(resCreated);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* DELETE: Eliminar una relacion Tiene por Viaje  */
+router.delete("/:vi_viaje", function (req, res, next) {
+  // OPERATION
+  let operation = "Eliminar una relacion Tiene por Viaje"
+  // ID
+  let { vi_viaje } = req.params;
+  // Condition
+  let condition = {
+    where: { vi_viaje }
+  };
+
+  modelRelTienes
+    .destroy(condition)
+    .then(() => {
+      let resUpdated = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: {}
+      };
+      res.status(200).json(resUpdated);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* DELETE: Eliminar una relacion Tiene por Usuario  */
+router.delete("/:us_usuario", function (req, res, next) {
+  // OPERATION
+  let operation = "Eliminar una relacion Tiene por Usuario"
+  // ID
+  let { us_usuario } = req.params;
+  // Condition
+  let condition = {
+    where: { us_usuario }
+  };
+
+  modelRelTienes
+    .destroy(condition)
+    .then(() => {
+      let resUpdated = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: {}
+      };
+      res.status(200).json(resUpdated);
     })
     .catch(err => {
       console.log(err);
