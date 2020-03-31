5789563958ac437b1b9c0b0a7f7866721fc2e5c6
Alejandro M. Coca <alxmcr@outlook.com>
2020-03-31 01:29:09 -0400

    + Crear las rutas para la actualizacion y eliminacion de un viaje

--- routes/api/v1/viajes.js
+++ routes/api/v1/viajes.js
@@ -103,4 +103,70 @@ router.post("/", function (req, res, next) {
     });
 });
 
+/* PUT: Actualizar  un(a) Viaje  */
+router.put("/:vi_viaje", function (req, res, next) {
+  // OPERATION
+  let operation = "Actualizar un(a) Viaje"
+  // ID
+  let { vi_viaje } = req.params;
+  // Request Data
+  let dataViajeToUpdate = req.body;
+  // Condition
+  let condition = {
+    where: { vi_viaje }
+  };
+
+  modelViaje
+    .update(dataViajeToUpdate, condition)
+    .then(viajeUpdated => {
+      let resUpdated = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: viajeUpdated
+      };
+      res.status(200).json(resUpdated);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
+/* DELETE: Eliminar de un(a) Viaje  */
+router.delete("/:vi_viaje", function (req, res, next) {
+  // OPERATION
+  let operation = "Eliminar un(a) Viaje"
+  // ID
+  let { vi_viaje } = req.params;
+  // Condition
+  let condition = {
+    where: { vi_viaje }
+  };
+
+  modelViaje
+    .destroy(condition)
+    .then(() => {
+      let resUpdated = {
+        estado: 200,
+        mensaje: `La operacion ${operation} de ${NAME_MODEL} fue un exito`,
+        data: {}
+      };
+      res.status(200).json(resUpdated);
+    })
+    .catch(err => {
+      console.log(err);
+      const errorBackend = {
+        estado: 500,
+        mensaje: `Ocurrio un error con el ${operation} de ${NAME_MODEL}`,
+        data: {}
+      }
+      res.status(500).send(errorBackend);
+    });
+});
+
 module.exports = router;
